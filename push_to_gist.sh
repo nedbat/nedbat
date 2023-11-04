69f7726eb7dfeb674b907d36d936f232b91cf970
Ned Batchelder <ned@nedbatchelder.com>
2023-11-04 07:05:56 -0400

    remember about credentials

--- push_to_gist.sh
+++ push_to_gist.sh
@@ -1,5 +1,7 @@
 # First, do this:
 #   gist -p README.md
+#       You might need to login first: gist --login
+#       The credentials are at ~/.gist if you want to remove them.
 # then take the id produced, and run:
 #   echo README.md | entr ./push_to_gist.sh abc123etc456
 
